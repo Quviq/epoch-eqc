2637410fcc984c5b46299612b5efe75747b9fe7b
Thomas Arts <thomas.arts@quviq.com>
2019-09-04 13:29:30 +0200

    Add auction field to state

--- aecore_eqc/txs_names_eqc.erl
+++ aecore_eqc/txs_names_eqc.erl
@@ -13,16 +13,21 @@
 
 -compile([export_all, nowarn_export_all]).
 
--define(NAMEFRAGS, ["foo", "bar", "baz"]).
+-define(NAMEFRAGS, ["foo", "bar", "baz", "longer-name",
+                    "31-bytes-minimum-as-auctionname",
+                    "this-name-is-32-bytes-ascii-name", "this-name-is-more-than-32-bytes-ascii-name"  %% do not enter auction
+                   ]).
 
 -record(preclaim,{name, salt, height, claimer, protocol, expires_by}).
--record(claim,{name, height, expires_by, protected_height, claimer, protocol}).
+-record(claim,{name, height, expires_by, claimer, protocol}).
+-record(auction, {name, height, expires_by, bid, claimer, protocol}).
 
 
 %% -- State and state functions ----------------------------------------------
 initial_state() ->
   #{preclaims => [],
     claims => [],
+    auctions => [],
     protected_names => [],
     named_accounts => #{}   %% Name can only be resolved if the Pointers contain "account_pubkey"
    }.
@@ -46,8 +51,10 @@ mine_next(#{height := Height} = S, _Value, [Blocks]) ->
   ExpiredClaims = expired_claims(S, Height + Blocks),
   ExpiredNames = [ {C#claim.name, C#claim.expires_by + aec_governance:name_protection_period()} || C <- ExpiredClaims ],
   ExpiredProtected = expired_protection(S, Height + Blocks),
+  ExpiredAuctions = expired_auctions(S, Height + Blocks),
   S#{preclaims => maps:get(preclaims, S, []) -- ExpiredPreclaims,
-     claims => maps:get(claims, S, []) -- ExpiredClaims,
+     claims => (maps:get(claims, S, []) -- ExpiredClaims) ++ [ auction_to_claim(A) || A <- ExpiredAuctions],
+     auctions => maps:get(auctions, S, []) -- ExpiredAuctions,
      protected_names =>  (maps:get(protected_names, S) -- ExpiredProtected ) ++
        [ {N, H} || {N, H} <- ExpiredNames, H >= Height + Blocks ],
      named_accounts => maps:without([ N || {N,_} <- ExpiredNames], maps:get(named_accounts, S, #{}))
@@ -56,9 +63,17 @@ mine_next(#{height := Height} = S, _Value, [Blocks]) ->
 mine_features(#{height := Height} = S, [Blocks], _Res) ->
   [{mine, expired_preclaims} || expired_preclaims(S, Height + Blocks) =/= [] ] ++
     [{mine, expired_claims} || expired_claims(S, Height + Blocks) =/= [] ] ++
+    [{mine, expired_auctions} || expired_auctions(S, Height + Blocks) =/= [] ] ++
     [{mine, expired_name_protection} || expired_protection(S, Height + Blocks) =/= [] ].
 
 
+%% Note that this does not put them in protected_names !!!
+auction_to_claim(#auction{name = Name, expires_by = Height, claimer = Claimer}) ->
+  #claim{name = Name,
+         height = Height,
+         expires_by = Height + aec_governance:name_claim_max_expiration(),
+         claimer = Claimer,
+         protocol = aec_hard_forks:protocol_effective_at_height(Height)}.
 
 
 %% --- Operation: ns_preclaim ---
@@ -123,52 +138,76 @@ ns_claim_args(S = #{height := Height}) ->
           name => Name,
           name_salt => Salt,
           fee => gen_fee(Height),
+          name_fee => oneof([prelima,
+                             20000000,  %% too little
+                             try aec_governance:name_claim_fee(Name, aec_hard_forks:protocol_effective_at_height(Height))
+                             catch _:_ -> 400000000
+                             end]),
           nonce => gen_nonce()}]).
 
 ns_claim_valid(S = #{height := Height}, [Sender, #{name := Name} = Tx]) ->
+  Protocol = aec_hard_forks:protocol_effective_at_height(Height),
   valid([{account, is_account(S, Sender)},
          {balance, check_balance(S, Sender, maps:get(fee, Tx))},
          {nonce, maps:get(nonce, Tx) == good},
          {fee, valid_fee(Height, Tx)},
-         {preclaim, is_valid_preclaim(S, Tx, Sender)},
-         {valid_name,  valid_name(Height, maps:get(name,Tx))},
+         {preclaim, is_valid_preclaim(Protocol, S, Tx, Sender)},  %% after Lima Salt distinguishes
+         {valid_name,  valid_name(Protocol, maps:get(name,Tx))},
          {unclaimed, not is_claimed(S, Name)},
-         {unprotected, not is_protected(S, Name)}]).
-
-is_valid_preclaim(#{preclaims := Ps, height := Height}, _Tx = #{name := Name, name_salt := Salt}, Claimer) ->
-  case [ PC || PC = #preclaim{name = N, salt = Sa, claimer = C} <- Ps,
-               Name == N, Salt == Sa, Claimer == C ] of
-    [] -> false;
-    [#preclaim{ height = H }] ->
-      H + aec_governance:name_claim_preclaim_delta() =< Height
-        andalso Height < H +  aec_governance:name_preclaim_expiration()  %% this is always the case in this model
-  end.
-
-%% names may not have dots in between, only at the end (.test)
-%% This also holds for Lima, creating subnames is a different transaction
-valid_name(Height, Name) ->
-  Protocol = aec_hard_forks:protocol_effective_at_height(Height),
-  case string:lexemes(Name, ".") of
-    [_, Registery] -> lists:member(Registery, aec_governance:name_registrars(Protocol));
-    _ -> false
-  end.
+         {unprotected, not is_protected(S, Name)},
+         {valid_name_fee, is_valid_name_fee(Protocol, Name, maps:get(name_fee, Tx))},
+         {valid_bid, true}]).
 
 ns_claim_tx(S, [Sender, Tx]) ->
-  aens_claim_tx:new(update_nonce(S, Sender, Tx)).
+  FixTx = case maps:get(name_fee, Tx) of
+            prelima -> maps:remove(name_fee, Tx);
+            _ -> Tx
+          end,
+  aens_claim_tx:new(update_nonce(S, Sender, FixTx)).
 
 ns_claim_next(S = #{height := Height}, _Value, [Sender, Tx] = Args) ->
   case ns_claim_valid(S, Args) of
     true  ->
-      #{ fee := Fee, name := Name } = Tx,
-      Claim = #claim{name    = Name,
-                     height  = Height,
-                     expires_by = Height + aec_governance:name_claim_max_expiration(),
-                     claimer = Sender,
-                     protocol = aec_hard_forks:protocol_effective_at_height(Height)},
-      remove_preclaim(Tx,
-                      reserve_fee(Fee,
-                                  bump_and_charge(Sender, Fee,
-                                                  add(claims, Claim, S))));
+      #{ fee := Fee, name := Name, name_salt := Salt } = Tx,
+      Protocol = aec_hard_forks:protocol_effective_at_height(Height),
+      case aec_governance:name_claim_bid_timeout(Name, Protocol) of
+        0 ->
+          Claim = #claim{name    = Name,
+                         height  = Height,
+                         expires_by = Height + aec_governance:name_claim_max_expiration(),
+                         claimer = Sender,
+                         protocol = Protocol},
+          NameFee = case maps:get(name_fee, Tx) of
+                      prelima -> 0; %% we don't have a locked account in the model
+                      X ->  X
+                    end,
+          remove_preclaim(Tx,
+                          reserve_fee(Fee,
+                                      bump_and_charge(Sender, Fee + NameFee,
+                                                      add(claims, Claim, S))));
+        Timeout ->
+          NameFee = maps:get(name_fee, Tx),
+          NewBid = #auction{name = Name,
+                            height = Height,
+                            expires_by = Height + Timeout,
+                            bid = NameFee,
+                            claimer = Sender,
+                            protocol = Protocol},
+          case lists:keyfind(Name, #auction.name, maps:get(auctions, S)) of
+            false ->
+              remove_preclaim(Tx,
+                              reserve_fee(Fee,
+                                          bump_and_charge(Sender, Fee + NameFee,
+                                                          add(auctions, NewBid, S))));
+            #auction{claimer = PrevBidder,
+                     bid = PrevBid} ->
+              reserve_fee(Fee,
+                          credit(PrevBidder, PrevBid,
+                                 bump_and_charge(Sender, Fee + NameFee,
+                                               add(auctions, NewBid,
+                                                   remove(auctions, Name, #auction.name, S)))))
+          end
+      end;
     _ -> S
   end.
 
@@ -180,6 +219,7 @@ ns_claim_features(S =  #{height := Height}, [_Sender, Tx] = Args, Res) ->
   Correct = ns_claim_valid(S, Args),
   [{correct, if Correct -> ns_claim; true -> false end},
    {ns_claim, Res}] ++
+    [{ns_claim, maps:get(name, Tx)} || Res == ok] ++
     [ {ns_claim, {protocol,
        aec_hard_forks:protocol_effective_at_height(Height) -
          get_preclaim_protocol(Tx,S)}} || Correct ].
@@ -208,9 +248,6 @@ ns_update_args(S = #{height := Height}) ->
                         %% This means that such a transaction cannot be created (which makes sense if serialization of it is undefined
               end || {Kind, Key} <- Pointers ]
          }]).
-
-ns_update_pre(#{height := Height}, [Name, _, _, _]) ->
-  backward_compatible(Height, Name).
 
 ns_update_valid(#{height := Height} = S, [Name, Sender, _Pointers, Tx]) ->
   valid([{account, is_account(S, Sender)},
@@ -258,9 +295,6 @@ ns_revoke_args(#{height := Height} = S) ->
              nonce => gen_nonce()
             }]).
 
-ns_revoke_pre(#{height := Height}, [_, Name, _]) ->
-  backward_compatible(Height, Name).
-
 ns_revoke_valid(#{height := Height} = S, [Sender, Name, Tx]) ->
   valid([{account, is_account(S, Sender)},
          {balance, check_balance(S, Sender, maps:get(fee, Tx))},
@@ -305,7 +339,7 @@ ns_transfer_args(#{height := Height} = S) ->
     ?LET({{Name, Sender}, To},
          {gen_claimed_name(S),
           oneof([{account, gen_account(1, 49, S)},
-                 {name, elements(maps:keys(maps:get(named_accounts, S, #{})) ++ [<<"ta.test">>])}])},
+                 {name, elements(maps:keys(maps:get(named_accounts, S, #{})) ++ [<<"ta.test">>, <<"ta.aet">>])}])},
          [Sender, To, Name,
           #{account_id => aeser_id:create(account, Sender),  %% The sender is asserted to never be a name.
             recipient_id =>
@@ -319,9 +353,6 @@ ns_transfer_args(#{height := Height} = S) ->
             fee => gen_fee(Height),
             nonce => gen_nonce()
            }]).
-
-ns_transfer_pre(#{height := Height}, [_, _, Name, _]) ->
-  backward_compatible(Height, Name).
 
 ns_transfer_valid(#{height := Height} = S, [Sender, To, Name, Tx]) ->
   valid([{account, is_account(S, Sender)},
@@ -453,6 +484,10 @@ expired_claims(S, Height) ->
   [ C || C <- maps:get(claims, S, []),
          C#claim.expires_by < Height ].
 
+expired_auctions(S, Height) ->
+  [ A || A <- maps:get(auctions, S, []),
+         A#auction.expires_by < Height ].
+
 expired_protection(S, Height) ->
   [ {N, H} || {N, H} <- maps:get(protected_names, S), H < Height ].
 
@@ -561,6 +596,40 @@ is_claimed_name(#{claims := Names}, Name, Height) ->
 is_protected(S, Name) ->
   proplists:get_value(Name, maps:get(protected_names, S)) =/= undefined.
 
+is_valid_name_fee(Protocol, Name, NameFee) when Protocol < 4 ->
+   NameFee == prelima;
+is_valid_name_fee(Protocol, Name, NameFee) ->
+  valid_name(Protocol, Name) andalso  %% otherwise fee computation may fail
+    is_integer(NameFee) andalso
+    NameFee >= aec_governance:name_claim_fee(Name, Protocol).
+
+is_valid_preclaim(Protocol, S,  #{name_salt := Salt} = Tx, Claimer) when Protocol < 4 ->
+  is_valid_preclaim_no_auction(S, Tx, Claimer);
+is_valid_preclaim(_Protocol, #{auctions := Auctions} = S, #{name := Name, name_salt := Salt} = Tx, Claimer) ->
+  %% Name should be in auction
+  case Salt == 0 of
+    true -> lists:keymember(Name, #auction.name, Auctions);
+    false -> not lists:keymember(Name, #auction.name, Auctions) andalso
+               is_valid_preclaim_no_auction(S, Tx, Claimer)
+  end.
+
+is_valid_preclaim_no_auction( #{preclaims := Ps, height := Height},
+                              #{name := Name, name_salt := Salt}, Claimer) ->
+  case [ PC || PC = #preclaim{name = N, salt = Sa, claimer = C} <- Ps,
+               Name == N, Salt == Sa, Claimer == C ] of
+    [] -> false;
+    [#preclaim{ height = H }] ->
+      H + aec_governance:name_claim_preclaim_delta() =< Height
+        andalso Height < H +  aec_governance:name_preclaim_expiration()  %% this is always the case in this model
+  end.
+
+%% names may not have dots in between, only at the end (.test)
+%% This also holds for Lima, creating subnames is a different transaction
+valid_name(Protocol, Name) ->
+  case string:lexemes(Name, ".") of
+    [_, Registery] -> lists:member(Registery, aec_governance:name_registrars(Protocol));
+    _ -> false
+  end.
 
 %% -- Generators -------------------------------------------------------------
 minimum_gas_price(H) ->
@@ -599,11 +668,12 @@ gen_claim(#{preclaims := Ps} = S) ->
               end)},
     {1, {gen_name(), gen_salt(), gen_account(1, 1, S)}}).
 
-gen_claimed_name(#{claims := []} = S) ->
+gen_claimed_name(#{claims := [], auctions := []} = S) ->
   {gen_name(), gen_account(1, 1, S)};
-gen_claimed_name(#{claims := Cs} = S) ->
+gen_claimed_name(#{claims := Cs, auctions := As} = S) ->
   weighted_default(
-    {39, ?LET(#claim{name = N, claimer = C}, elements(Cs),
+    {39, ?LET({N, C}, elements([ {N, C} || #claim{name = N, claimer = C} <- Cs ] ++
+                                 [ {N, C} || #auction{name = N, claimer = C} <- As]),
               begin
                 frequency([{1, {gen_name(), C}}, {1, {N, gen_account(0, 1, S)}}, {38, {N, C}}])
               end)},
